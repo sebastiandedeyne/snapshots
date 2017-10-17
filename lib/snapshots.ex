9d7ea1cba08f5bfb5593039001f7f87fa3a777e5
Sebastian De Deyne <sebastiandedeyne@gmail.com>
2017-10-17 08:44:19 +0200

    First commit

---
+++ lib/snapshots.ex
@@ -0,0 +1,18 @@
+defmodule Snapshots do
+  @moduledoc """
+  Documentation for Snapshots.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Snapshots.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/snapshots_test.exs
@@ -0,0 +1,8 @@
+defmodule SnapshotsTest do
+  use ExUnit.Case
+  doctest Snapshots
+
+  test "greets the world" do
+    assert Snapshots.hello() == :world
+  end
+end
