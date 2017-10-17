b405cd10eb8057059e336467b1b58bdb6afe419e
Sebastian De Deyne <sebastiandedeyne@gmail.com>
2017-10-17 22:46:42 +0200

    Add test for snapshot create

--- test/snapshots_test.exs
+++ test/snapshots_test.exs
@@ -3,7 +3,17 @@ defmodule SnapshotsTest do
   use Snapshots
   doctest Snapshots
 
-  test "greets the world" do
+  test "assert an existing snapshot" do
     assert_snapshot "Hello, world!"
+  end
+
+  test "create a new snapshot" do
+    assert_snapshot "Hello, world!"
+
+    path = "test/__snapshots__/create_a_new_snapshot.txt"
+
+    assert File.exists?(path) == true
+
+    File.rm!(path)
   end
 end
