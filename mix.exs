283487f56e43bc141b966ef7a0ea34bbfcd3335d
Sebastian De Deyne <sebastiandedeyne@gmail.com>
2017-10-26 21:08:55 +0200

    Update mix manifest

--- mix.exs
+++ mix.exs
@@ -1,28 +1,42 @@
 defmodule Snapshots.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+
   def project do
     [
       app: :snapshots,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.5",
       start_permanent: Mix.env == :prod,
-      deps: deps()
+      description: "Snapshot assertions for ExUnit.",
+       package: package(),
+       deps: deps(),
+       docs: [
+         extras: ["README.md"],
+         main: "readme",
+         source_ref: "v#{@version}",
+         source_url: "https://github.com/sebastiandedeyne/snapshots"]
     ]
   end
 
   # Run "mix help compile.app" to learn about applications.
   def application do
+    []
+  end
+
+  defp package do
     [
-      extra_applications: [:logger]
+      name: :snapshots,
+      files: ["lib", "mix.exs", "README.md", "LICENSE.md"],
+      maintainers: ["Sebastian De Deyne"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/sebastiandedeyne/snapshots"}
     ]
   end
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
-    [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
-    ]
+    []
   end
 end
